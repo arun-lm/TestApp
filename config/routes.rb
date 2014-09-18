a2604f9f30c8947a603527a8c4ce27cb6fbae02b
Arun <arunm@logicmatter.com>
2014-09-18 13:10:44 +0530

    First version of User models and Controllers created and committing now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SecondApp::Application.routes.draw do
+  resources :users
+  #get '/users/' => 'users#index'
+
+  #get '/users/new' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
